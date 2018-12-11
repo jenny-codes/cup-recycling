d83961445804144a155d36262be4a7615a67bdc8
Jenny Shih <jinghua.shih@gmail.com>
2018-12-11 13:45:22 +0800

    新增 user login/logout flow

--- interaction/views.py
+++ interaction/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render, get_object_or_404
-from interaction.models import Cup, Business, User
+from interaction.models import Cup, Business, User, Machine
+
 # from django.views import generic
+
+# function_based login restriction
+# from django.contrib.auth.decorators import login_required
+# @login_required
+
+# class_based login restriction
+# from django.contrib.auth.mixins import LoginRequiredMixin
+# class MyView(LoginRequiredMixin, View):
 
 def index(request):
 
