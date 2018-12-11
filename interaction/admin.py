033a6565f37ef53f648b260385be1f56e1ac732d
Jenny Shih <jinghua.shih@gmail.com>
2018-12-11 12:59:33 +0800

    整理 model, 新增 admin

--- interaction/admin.py
+++ interaction/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from interaction.models import Cup, Business, User, Machine
 
-# Register your models here.
+# does the same thing as admin.site.register(Cup, CupAdmin)
+@admin.register(Cup)
+class CupAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'status', 'owner_type', 'owner_id')
+    list_filter = ('owner_type', 'status')
+
+@admin.register(Business)
+class BusinessAdmin(admin.ModelAdmin):
+    list_display = ('name', 'phone', 'email', 'active')
+    fieldsets = (
+        ('Basic Information', {
+            'fields': ('name', 'phone','email', 'address')
+        }),
+        (None, {
+            'fields': ('active',)
+        }),
+    )
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('name', 'phone', 'email', 'active')
+    fieldsets = (
+        ('Basic Information', {
+            'fields': ('name', 'phone','email')
+        }),
+        (None, {
+            'fields': ('active',)
+        }),
+    )
+
+@admin.register(Machine)
+class MachineAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'location', 'active')
--- interaction/models.py
+++ interaction/models.py
@@ -1,87 +1,103 @@
 from django.db import models
 from django.urls import reverse # Used to generate URLs by reversing the URL patterns
 
+""" Manager Classes """
+class CupManager(models.Manager):
+    def get_queryset(self):
+        return super(CupManager, self).get_queryset().exclude(status='n')
+
+class CupInBusinessManager(models.Manager):
+    def get_queryset(self):
+        return super(CupInBusinessManager, self).get_queryset().exclude(owner_type='b')   
+
+class CupInUserManager(models.Manager):
+    def get_queryset(self):
+        return super(CupInUserManager, self).get_queryset().exclude(owner_type='u')  
+
+class CupInMachineManager(models.Manager):
+    def get_queryset(self):
+        return super(CupInMachineManager, self).get_queryset().exclude(owner_type='m')   
+
+class CupInAdminManager(models.Manager):
+    def get_queryset(self):
+        return super(CupInAdminManager, self).get_queryset().exclude(owner_type='a')   
+
+
 class Cup(models.Model):
+    # TODO: RFID 
+    objects = models.Manager()
+    in_circulation = CupManager()
+    in_business = CupInBusinessManager()
+    in_user = CupInUserManager()
+    in_machine = CupInMachineManager()
+    in_admin = CupInAdminManager()
 
     LOAN_STATUS = (
         ('o', 'On loan'),
+        ('c', 'Cleaning'),
         ('a', 'Available'),
-        ('d', 'Discarded'),
+        ('n', 'Not in circulation'),
         ('r', 'Returned, to be cleaned')
     )
-
     status = models.CharField(
         max_length=1,
         choices=LOAN_STATUS,
         default='r',
         help_text='Cup availability',
     )
-    
-    OWNER_TYPE = (
+    OWNER_TYPES = (
         ('b', 'Business'),
         ('u', 'User'),
-        ('a', 'Administration')
+        ('a', 'Administration'),
+        ('m', 'Machine')
     )
+    owner_type = models.CharField(max_length=1, choices = OWNER_TYPES, default='a')
+    owner_id = models.IntegerField(null = True, blank = True)
 
-    business = models.ForeignKey(
-                    'Business',
-                    models.SET_NULL,
-                    blank=True,
-                    null=True,
-                )
-
-    user = models.ForeignKey(
-                'User',
-                models.SET_NULL,
-                blank=True,
-                null=True,
-            )
-
-    # A cup is either in an user's or a business' hand
-    owner_type = models.CharField(
-        max_length=1,
-        choices=OWNER_TYPE,
-    )
-
-    def get_owner(self, owner_type, pk):
-        switcher = {
-            'b': Business.filter(id=pk),
-            'u': User.filter(id=pk)
-        }
-        return switcher.get(owner_type, None)
+    def owner(self):
+        if self.owner_type == 'b':
+            return Business.objects.get(pk = self.owner_id)
+        elif self.owner_type == 'u':
+            return User.objects.get(pk= self.owner_id)
+        elif self.owner_type == 'm':
+            return Machine.objects.get(pk = self.owner_id)
+        return None
 
     def __str__(self):
-        return self.id
-        
-
+        return str(self.pk)
+    
 class Owner(models.Model):
     name = models.CharField(max_length=50, help_text='Enter a business\' brand name or a user\'s name.')
     email = models.EmailField()
     phone = models.CharField(max_length=10, help_text='e.g. 0912345678')
-
-    def __str__(self):
-        return self.name
+    active = models.BooleanField(default=True)
 
     def get_absolute_url(self):
         """Returns the url to access a detail record for this owner."""
         return reverse('owner-detail', args=[str(self.id)])
+    def __str__(self):
+        return self.name
 
     class Meta:
         abstract = True
 
-
 class Business(Owner):
-
+    # objects = BusinessManager()
     address = models.CharField(max_length=300)
 
-    def count_cups(self):
-        """Returns the respective number of available/returned cups currently at this place."""
-        cups = {
-            'a': self.cup.filter(status__exact='a').count(),
-            'r': self.cup.filter(status__exact='r').count(),
-        }
-        return cups
+    def cups(self):
+        return Cup.objects.filter(owner_type = 'b', owner_id = self.id)
 
 class User(Owner):
-    pass
+    # objects = UserManager()
+    def cups(self):
+        return Cup.objects.filter(owner_type = 'u', owner_id = self.id)
+
+class Machine(models.Model):
+    # objects = MachineManager()
+    location = models.CharField(max_length=300)
+    active = models.BooleanField(default=True)
+    
+    def cups(self):
+        return Cup.objects.filter(owner_type = 'm', owner_id = self.id)
 
