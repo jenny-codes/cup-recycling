a62fccae9c6fb9c8cf27070b94cc4e817fcfad0f
Jenny Shih <jinghua.shih@gmail.com>
2018-12-17 18:45:03 +0800

    匯入 html 首版

---
+++ interaction/static/js/index.js
@@ -0,0 +1,17 @@
+window.onscroll = function() {myFunction()};
+
+var navbar = document.querySelector(".navbar");
+var navbar_height = navbar.offsetHeight;
+
+function myFunction() {
+  if (window.pageYOffset >= navbar_height) {
+    navbar.classList.add("navbar_fixed")
+  } else {
+    navbar.classList.remove("navbar_fixed");
+  }
+}
+
+document.querySelector('#nav-toggle').addEventListener('click',function(){
+  document.querySelector('.menu').classList.toggle('toggle_menu')
+})
+
