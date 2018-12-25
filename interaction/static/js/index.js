02ecc77aa672314424976d2ead3e2de637936bb6
Jenny Shih <jinghua.shih@gmail.com>
2018-12-25 20:10:39 +0800

    new style

--- interaction/static/js/index.js
+++ interaction/static/js/index.js
@@ -1,7 +1,6 @@
 window.onscroll = function() {myFunction()};
 
 var navbar = document.querySelector(".navbar");
-console.log(navbar);
 var navbar_height = navbar.offsetHeight;
 
 function myFunction() {
@@ -16,27 +15,5 @@ document.querySelector('#nav-toggle').addEventListener('click',function(){
   document.querySelector('.menu').classList.toggle('toggle_menu')
 })
 
-// $(document).ready(function() {
-//     $('dropdown-toggle').dropdown()
-// });
 
-
-$(document).ready(function() {
-  $(".mySlides:first").fadeIn()
-});
-
-$( ".mySlides" ).click(function() {
-  console.log($( this ))
-  console.log($(this).next().length)
-  if ( $( this ).next().length != 0 ){
-    $( this ).slideUp();
-    $( this ).next().slideDown();
-  } else{  
-    $("html, body").scrollTop($("#sec_cooperation").offset().top);
-  }
-});
-
-// animate function: https://api.jquery.com/animate/
-// traversing: https://www.w3schools.com/jquery/jquery_ref_traversing.asp
-// geolocation: https://developer.mozilla.org/en-US/docs/Web/Apps/Fundamentals/gather_and_modify_data/Plotting_yourself_on_the_map
 
