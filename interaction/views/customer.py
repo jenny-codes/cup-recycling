141036c5bf1b46c098a42ddd6f2198a54a7416b3
Jenny Shih <jinghua.shih@gmail.com>
2018-12-25 19:10:29 +0800

    modulize views

---
+++ interaction/views/customer.py
@@ -0,0 +1,17 @@
+from interaction.views import * 
+
+class CustomerRecordListView(LoginRequiredMixin, ListView):
+    """列出使用者的租借杯紀錄"""
+    model = Record
+    template_name ='customer_record.html'
+    context_object_name = 'checked_out_records' # default is 'record_list'
+    paginate_by = 10
+    
+    def get_queryset(self):
+        return Record.objects.filter(user=self.request.user).order_by('loaned_out_at')
+
+    # def get_context_data(self, **kwargs):
+    #     context = super(CustomerRecordListView, self).get_context_data(**kwargs)
+    #     cups_returned = Record.objects.filter(source=self.request.user).order_by('timestamp')
+    #     context['returned_records'] = cups_returned
+    #     return context
---
+++ interaction/views/registration.py
@@ -0,0 +1,32 @@
+from interaction.views import * 
+from django.views.generic import CreateView
+
+class BusinessSignUpView(CreateView):
+    model = CupUser
+    form_class = BusinessSignUpForm
+    template_name = 'registration/signup_form.html'
+
+    def get_context_data(self, **kwargs):
+        kwargs['user_type'] = 'business'
+        return super().get_context_data(**kwargs)
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('/')
+
+class CustomerSignUpView(CreateView):
+    model = CupUser
+    form_class = CustomerSignUpForm
+    template_name = 'registration/signup_form.html'
+
+    def get_context_data(self, **kwargs):
+        print('get_context_data')
+        kwargs['user_type'] = 'customer'
+        return super().get_context_data(**kwargs)
+
+    def form_valid(self, form):
+        print('form_valid')
+        user = form.save()
+        login(self.request, user)
+        return redirect('/')
