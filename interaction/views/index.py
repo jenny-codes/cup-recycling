d13dbb8c94e8a7ced26f3b418b4b06b9bf77dac9
Jenny Shih <jinghua.shih@gmai.com>
2019-01-03 13:46:08 +0800

    add stats to customer record

--- interaction/views/index.py
+++ interaction/views/index.py
@@ -1,7 +1,4 @@
 from interaction.views import render
-from IPython import embed
-def index(request):
 
-    context = {}
-    embed()
-    return render(request, 'index.html', context=context)
+def index(request):
+    return render(request, 'index.html', context={})
