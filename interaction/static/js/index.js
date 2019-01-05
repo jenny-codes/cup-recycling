a31fcce89d6c286a4291b69a35bbd9f7f091e9e6
Jenny Shih <jinghua.shih@gmai.com>
2019-01-05 14:17:30 +0800

    restructured js files

--- interaction/static/js/index.js
+++ interaction/static/js/index.js
@@ -1,35 +1,7 @@
 // 畫面滑動會發生的事情
 window.onscroll = function(){
-  scrollNav();
   toTopShow();
 }
-
-// 畫面滑動後navbar固定
-var prevScrollpos = window.pageYOffset;
-function scrollNav() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.querySelector(".navbar").style.top = "0";
-  } else {
-    document.querySelector(".navbar").style.top = "-50px";
-  }
-  prevScrollpos = currentScrollPos;
-}
-
-// 手機版點選漢堡後，menu出現
-document.querySelector('#nav-toggle').addEventListener('click',function(){
-  document.querySelector('.menu').classList.toggle('toggle_menu')
-})
-
-// navbar下拉選單出現，並且可點選
-document.querySelector('#navbarDropdown').addEventListener('mouseover',function(){
-  document.querySelector('.dropdown-menu').classList.toggle('show');
-  var dropdownItem = document.querySelectorAll('.dropdown-item');
-  for(i=0;i<dropdownItem.length;i++){
-    dropdownItem[i].classList.toggle('click');
-    // pointEvent = none/auto;
-  }
-})
 
 var top_button = document.getElementById("top_button");
 function toTopShow(){
---
+++ interaction/static/js/nav_bar.js
@@ -0,0 +1,31 @@
+// 畫面滑動會發生的事情
+window.onscroll = function(){
+  scrollNav();
+}
+
+// 畫面滑動後navbar固定
+var prevScrollpos = window.pageYOffset;
+function scrollNav() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.querySelector(".navbar").style.top = "0";
+  } else {
+    document.querySelector(".navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+// 手機版點選漢堡後，menu出現
+document.querySelector('#nav-toggle').addEventListener('click',function(){
+  document.querySelector('.menu').classList.toggle('toggle_menu')
+})
+
+// navbar下拉選單出現，並且可點選
+document.querySelector('#navbarDropdown').addEventListener('mouseover',function(){
+  document.querySelector('.dropdown-menu').classList.toggle('show');
+  var dropdownItem = document.querySelectorAll('.dropdown-item');
+  for(i=0;i<dropdownItem.length;i++){
+    dropdownItem[i].classList.toggle('click');
+    // pointEvent = none/auto;
+  }
+})
